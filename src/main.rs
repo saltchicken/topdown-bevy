a41432e8d950be458c44502b4c178a9485c8cd72
saltchicken <john.eicher89@gmail.com>
2026-06-05 15:45:46 -0700

    Add window size and title

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,14 @@ use state::GameState;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (1280.0, 720.0).into(),
+                title: "topdown".into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .init_state::<GameState>()
         .add_plugins((MenuPlugin, PlayerPlugin))
         .run();
