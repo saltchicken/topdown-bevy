44696fe28b5a2e52d04d56bc15bc14ec13d916ad
saltchicken <john.eicher89@gmail.com>
2026-06-05 20:52:34 -0700

    Tag MainCamera

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,11 @@ const WINDOW_WIDTH: f32 = 1280.0;
 const WINDOW_HEIGHT: f32 = 720.0;
 const WINDOW_TITLE: &str = "topdown";
 
+#[derive(Component)]
+pub struct MainCamera;
+
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((Camera2dBundle::default(), MainCamera));
 }
 
 fn main() {
