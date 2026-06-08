d78f541c903f19029a1434123c5d928c3b688e50
saltchicken <john.eicher89@gmail.com>
2026-06-08 15:09:10 -0700

    Use DualAxis

--- src/input.rs
+++ src/input.rs
@@ -4,20 +4,15 @@ use leafwing_input_manager::prelude::*;
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
 pub enum PlayerAction {
     Toggle,
-    Up,
-    Down,
-    Left,
-    Right,
+    #[actionlike(DualAxis)]
+    Move,
 }
 
 impl PlayerAction {
     pub fn default_input_map() -> InputMap<Self> {
         InputMap::default()
             .with(Self::Toggle, KeyCode::Space)
-            .with(Self::Up, KeyCode::KeyW)
-            .with(Self::Down, KeyCode::KeyS)
-            .with(Self::Left, KeyCode::KeyA)
-            .with(Self::Right, KeyCode::KeyD)
+            .with_dual_axis(Self::Move, VirtualDPad::wasd())
     }
 }
 
